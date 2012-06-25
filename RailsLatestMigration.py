3b2b1f702a3c7223f8516affe0a0a347bc3f67ba
Alex Plescan <alexpls@gmail.com>
2012-06-25 21:41:13 +1200

    Init

---
+++ RailsLatestMigration.py
@@ -0,0 +1,42 @@
+import sublime, sublime_plugin
+import os
+import re
+
+class RailsLatestMigrationCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		cur_path = self.parent_path(self.view.file_name())
+		root = self.find_ror_root(cur_path)
+
+		if root:
+			migrations_dir = os.path.join(root, 'db', 'migrate')
+			migrations = os.listdir(migrations_dir)
+
+			pattern = re.compile('^\d+_\w+.rb$')
+			migrations = sorted([m for m in migrations if pattern.match(m)])
+			latest_migration = os.path.join(migrations_dir, migrations[-1])
+
+			self.view.window().open_file(latest_migration)
+
+	# Recursively searches each up a directory structure for the 
+	# expected items that are common to a Rails application.
+	def find_ror_root(self, path):
+		expected_items = ['Gemfile', 'app', 'config', 'db']
+		files = os.listdir(path)
+
+		# The recursive search has gone too far and we've reached the system's
+		# root directory! At this stage it's safe to assume that we won't come
+		# across a familiar directory structure for a Rails app.
+		if path == '/':
+			raise NotRailsApp
+
+		if len([x for x in expected_items if x in files]) == len(expected_items):
+			return path
+		else:
+			return self.find_ror_root(self.parent_path(path))
+
+	# Returns the parent path of the path passed to it.
+	def parent_path(self, path):
+		return os.path.abspath(os.path.join(path, '..'))
+
+class NotRailsApp(Exception):
+	pass
---
+++ Readme.markdown
@@ -0,0 +1,15 @@
+#Rails Latest Migration
+
+Opens the latest migration created in a Ruby on Rails app.
+
+##Usage
+
+Easy! 
+
+1. Bring up your command pallette (Cmd + Shift + P / Ctrl + Shift + P) and type in:
+```
+latest
+```
+2. In no time the "Rails Latest Migration: Open latest Rails migration" should be highlighted, like so:
+![Rails Latest Migration Screenshot](https://github.com/alexpls/Rails-Latest-Migration/raw/gh-pages/screenshots/screenshot_1.png)
+3. Hit enter, and lo and behold; your latest migration!
